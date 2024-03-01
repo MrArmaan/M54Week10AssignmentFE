d43516cb14a60bdce7a96bfb9e46269bb5c8b784
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-03-01 12:41:10 +0000

    linked fe

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5001";
+const API_BASE_URL = "https://m54week10assginmentbe.onrender.com/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
